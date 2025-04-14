812acc6d0ee033865656fa06b835662be0d94e74
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-04-14 10:16:53 +0300

    output objects works

---
+++ constants/tasks.js
@@ -0,0 +1,34 @@
+const tasks = [
+  {
+    id: 1,
+    expense: "cement",
+    description: "Cement for the foundation",
+    category: "construction",
+    amount: 500,
+    date: "2023-10-01",
+  },
+  {
+    id: 2,
+    expense: "bricks",
+    description: "Bricks for the walls",
+    category: "construction",
+    amount: 300,
+    date: "2023-10-02",
+  },
+  {
+    id: 3,
+    expense: "paint",
+    description: "Paint for the interior",
+    category: "finishing",
+    amount: 200,
+    date: "2023-10-03",
+  },
+  {
+    id: 4,
+    expense: "roofing",
+    description: "Roofing materials",
+    category: "construction",
+    amount: 800,
+    date: "2023-10-04",
+  },
+];
